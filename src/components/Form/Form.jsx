462b9ba664752a4a75ab08351d299ab8b51af008
Lucas <lucas_rodrigues1@yahoo.com.br>
2022-04-16 20:07:47 -0300

    update: create popups cards

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,5 +1,8 @@
 import { useState } from "react";
-import "./styles.css"
+import "./styles.css";
+import { ToastContainer, toast } from "react-toastify";
+
+import "react-toastify/dist/ReactToastify.css";
 
 const Form = ({ listTransactions, setListTransactions }) => {
   const [description, setDescription] = useState("");
@@ -14,58 +17,79 @@ const Form = ({ listTransactions, setListTransactions }) => {
   const data = {
     description,
     value,
-    type
-  }
+    type,
+  };
 
   return (
     <>
-        <form onSubmit={(event) => event.preventDefault()}>
-          <label className="lableDescription" htmlFor="Descrição">
-            Descrição
-            <input className="inputDescription"
-              placeholder="Digite aqui sua descrição"
-              id="Descrição"
-              type="text"
-              value={description}
-              onChange={(event) => setDescription(event.target.value)}
-              required
-            ></input>
-          </label>
-          <h2 className="titleEx">Ex: Compra Roupas</h2>
-          <div className="inputsValueType">
-            <div className="inputValue">
-          <label htmlFor="valueForm">
-            Valor
-            <input className="valueInput"
-              id="valueForm"
-              placeholder="1 R$"
-              type="number"
-              value={value}
-              onChange={(event) => setValue(event.target.value)}
-              required
-            ></input>
-          </label>
+      <ToastContainer
+        position="bottom-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+      />
+      <form onSubmit={(event) => event.preventDefault()}>
+        <label className="lableDescription" htmlFor="Descrição">
+          Descrição
+          <input
+            className="inputDescription"
+            placeholder="Digite aqui sua descrição"
+            id="Descrição"
+            type="text"
+            value={description}
+            onChange={(event) => setDescription(event.target.value)}
+            required
+          ></input>
+        </label>
+        <h2 className="titleEx">Ex: Compra Roupas</h2>
+        <div className="inputsValueType">
+          <div className="inputValue">
+            <label htmlFor="valueForm">
+              Valor
+              <input
+                className="valueInput"
+                id="valueForm"
+                placeholder="1 R$"
+                type="number"
+                value={value}
+                onChange={(event) => setValue(event.target.value)}
+                required
+              ></input>
+            </label>
           </div>
           <div className="inputType">
-          <label>
-            Tipo do valor
-            <select className="typeInput" value={type} required onChange={(e) => setType(e.target.value)}>
-              {selects.map((item, index) => (
-                <option value={item.id}>{item.name}</option>
-              ))}
-            </select>
-          </label>       
+            <label>
+              Tipo do valor
+              <select
+                className="typeInput"
+                value={type}
+                required
+                onChange={(e) => setType(e.target.value)}
+              >
+                {selects.map((item, index) => (
+                  <option value={item.id}>{item.name}</option>
+                ))}
+              </select>
+            </label>
           </div>
-          </div>
-          <button className="buttonInput"
-            type="submit"
-            onClick={() => {
-              listTransactions(data);
-            }}
-          >
-            Inserir valor
-          </button>
-        </form>
+        </div>
+        <button
+          className="buttonInput"
+          type="submit"
+          onClick={() => {
+            data.description.length > 0 && listTransactions(data);
+            data.description.length > 0 && data.type === "Entrada" && toast.success(`Entrada adicionada com sucesso :D`)
+            data.description.length > 0 && data.type === "Despesa" && toast.error(`Despesa adicionada com sucesso`);
+          }}
+        >
+          Inserir valor
+        </button>
+      </form>
     </>
   );
 };
